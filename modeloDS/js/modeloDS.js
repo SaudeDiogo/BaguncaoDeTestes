6b19066519ee37e667b93ae428bee41f97b2c9af
SaudeDiogo <diogosaude0@gmail.com>
2018-09-26 17:56:39 -0300

    corrigindo alguns codigos e atualizando os link para dev

--- modeloDS/js/modeloDS.js
+++ modeloDS/js/modeloDS.js
@@ -2,7 +2,8 @@
 Criado por Diogo Saúde
 JS para modelo de turma
 Modelo DS
-v1.0
+producao    https://cdn.rawgit.com/SaudeDiogo/SyncMe/SyncMe/modeloDS/js/modeloDS.js
+dev         https://rawgit.com/SaudeDiogo/SyncMe/SyncMe/modeloDS/js/modeloDS.js
 **************************/
 
 // inserindo modelo-DS em todas as paginas da turma ModeloDS
@@ -10,7 +11,7 @@ v1.0
 var css = document.createElement('link');
 css.setAttribute('rel', 'stylesheet');
 css.setAttribute('type', 'text/css');
-css.setAttribute('href', 'https://cdn.rawgit.com/SaudeDiogo/SyncMe/SyncMe/modeloDS/css/curso.css');
+css.setAttribute('href', 'https://rawgit.com/SaudeDiogo/SyncMe/SyncMe/modeloDS/css/curso.css');
 //css.setAttribute('href', 'https://iesb.blackboard.com/bbcswebdav/institution/Ead/_disciplinas/_comum/css/modeloDS/curso.css');
 css.setAttribute('media', 'screen, print');
 document.getElementsByTagName('head')[0].appendChild(css); // fim da criação do css da sala modeloDS
@@ -24,7 +25,7 @@ document.getElementsByTagName('head')[0].appendChild(css); // fim da criação d
 var css = document.createElement('link');
 css.setAttribute('rel', 'stylesheet');
 css.setAttribute('type', 'text/css');
-css.setAttribute('href', 'https://cdn.rawgit.com/SaudeDiogo/SyncMe/SyncMe/modeloDS/css/pagina-inicial.css');
+css.setAttribute('href', 'https://rawgit.com/SaudeDiogo/SyncMe/SyncMe/modeloDS/css/pagina-inicial.css');
 //css.setAttribute('href', 'https://iesb.blackboard.com/bbcswebdav/institution/Ead/_disciplinas/_comum/css/modeloDS/pagina-inicial.css');
 css.setAttribute('media', 'screen, print');
 document.getElementsByTagName('head')[0].appendChild(css);
@@ -34,7 +35,7 @@ document.body.classList.add('modeloDS'); //inserindo classe do modeloDS
 
 
 
-if (document.querySelector('title').innerHTML == "Início – EAD-ModeloDS" ) {
+if (document.querySelector('title').innerHTML === "Início – EAD-ModeloDS") {
     document.querySelector('.shadow').classList.add('pagina-inicial'); //inserindo classe da pagina inicial
 }
 
