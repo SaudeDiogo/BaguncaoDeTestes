86dc79b96a68e3ae14def52c8d029a1c504b190d
SaudeDiogo <diogosaude0@gmail.com>
2018-09-26 16:22:05 -0300

    inserido links de rawgit no js que é executado na sala modeloDS

--- modeloDS/js/modeloDS.js
+++ modeloDS/js/modeloDS.js
@@ -6,18 +6,26 @@ v1.0
 **************************/
 
 //************************
-// inserindo modelo-DS
+// inserindo modelo-DS em todas as paginas da turma ModeloDS
 var css = document.createElement('link');
 css.setAttribute('rel', 'stylesheet');
 css.setAttribute('type', 'text/css');
-css.setAttribute('href', 'https://iesb.blackboard.com/bbcswebdav/institution/Ead/_disciplinas/_comum/css/modeloDS/curso.css');
+css.setAttribute('href', 'https://cdn.rawgit.com/SaudeDiogo/SyncMe/SyncMe/modeloDS/css/curso.css');
+//css.setAttribute('href', 'https://iesb.blackboard.com/bbcswebdav/institution/Ead/_disciplinas/_comum/css/modeloDS/curso.css');
 css.setAttribute('media', 'screen, print');
 document.getElementsByTagName('head')[0].appendChild(css); // fim da criação do css da sala modeloDS
 
+
+
+
+
+//executa apenas na página inicío da turma modeloDS
+//
 var css = document.createElement('link');
 css.setAttribute('rel', 'stylesheet');
 css.setAttribute('type', 'text/css');
-css.setAttribute('href', 'https://iesb.blackboard.com/bbcswebdav/institution/Ead/_disciplinas/_comum/css/modeloDS/pagina-inicial.css');
+css.setAttribute('href', 'https://cdn.rawgit.com/SaudeDiogo/SyncMe/SyncMe/modeloDS/css/pagina-inicial.css');
+//css.setAttribute('href', 'https://iesb.blackboard.com/bbcswebdav/institution/Ead/_disciplinas/_comum/css/modeloDS/pagina-inicial.css');
 css.setAttribute('media', 'screen, print');
 document.getElementsByTagName('head')[0].appendChild(css);
 
@@ -32,12 +40,8 @@ if (document.querySelector('title').innerHTML == "Início – EAD-ModeloDS" ) {
 
 
 
-if (document.querySelectorAll('body.modeloDS .pagina-inicial ul#content_listContainer li.clearfix.read div.item.clearfix h3 a span').innerHTML == "Aulas" ) {
-    document.querySelectorAll('body.modeloDS .pagina-inicial ul#content_listContainer li.clearfix.read').classList.add('cartao-aulas');
-}
 
 
-
-//************************
 // inicio personalização da turma
+//
 //document.getElementById('courseMapButton').addClass('hideoff'); //esconder icones
