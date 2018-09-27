4ea177ebba73ddf28327fbf587f118c2a32457d1
SaudeDiogo <diogosaude0@gmail.com>
2018-09-26 23:47:34 -0300

    .

--- modeloDS/js/modeloDS.js
+++ modeloDS/js/modeloDS.js
@@ -33,12 +33,11 @@ if (document.querySelector('title').innerHTML === "Início – EAD-ModeloDS") {
     document.querySelector('.shadow').classList.add('pagina-inicial'); //inserindo classe da pagina inicial
 }
 
-object.onload = function(){
+
 
 if (document.getElementById(anonymous_element_18).innerHTML === "Aulas") {
     document.getElementById(anonymous_element_18).classList.add('aulas');
 }
-};
 // inicio personalização da turma
 //
 //document.getElementById('courseMapButton').addClass('hideoff'); //esconder icones
