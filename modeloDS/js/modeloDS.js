7f5b403b5ff38d20a4f8fef00e3c673ce2dd9e3b
SaudeDiogo <diogosaude0@gmail.com>
2018-09-27 16:27:00 -0300

    .

--- modeloDS/js/modeloDS.js
+++ modeloDS/js/modeloDS.js
@@ -12,7 +12,7 @@ var css = document.createElement('link');
 css.setAttribute('rel', 'stylesheet');
 css.setAttribute('type', 'text/css');
 css.setAttribute('href', 'https://rawgit.com/SaudeDiogo/SyncMe/SyncMe/modeloDS/css/curso.css');
-//css.setAttribute('href', 'https://iesb.blackboard.com/bbcswebdav/institution/Ead/_disciplinas/_comum/css/modeloDS/curso.css');
+//css.setAttribute('href', 'https://iesb.blackboard.com/bbcswebdav/institution/Ead/_disciplinas/_comum/modeloDS/curso.css'); //diretorio local
 css.setAttribute('media', 'screen, print');
 document.getElementsByTagName('head')[0].appendChild(css); // fim da criação do css da sala modeloDS
 
@@ -29,7 +29,7 @@ if (document.querySelector('title').innerHTML === "Início – EAD-ModeloDS") {
     css.setAttribute('rel', 'stylesheet');
     css.setAttribute('type', 'text/css');
     css.setAttribute('href', 'https://rawgit.com/SaudeDiogo/SyncMe/SyncMe/modeloDS/css/pagina-inicial.css');
-    //css.setAttribute('href', 'https://iesb.blackboard.com/bbcswebdav/institution/Ead/_disciplinas/_comum/css/modeloDS/pagina-inicial.css');
+    //css.setAttribute('href', 'https://iesb.blackboard.com/bbcswebdav/institution/Ead/_disciplinas/_comum/modeloDS/pagina-inicial.css'); //diretorio local
     css.setAttribute('media', 'screen, print');
     document.getElementsByTagName('head')[0].appendChild(css);
 
